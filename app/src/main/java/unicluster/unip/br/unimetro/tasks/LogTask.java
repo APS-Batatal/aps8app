f634c33f6037c44fb0575e241b266dd570234367
Diego Lopes <dihgg@dihgg.com>
2017-11-04 14:33:48 -0200

    referência de pacotes e imagens

--- app/src/main/java/unicluster/unip/br/unimetro/tasks/LogTask.java
+++ app/src/main/java/unicluster/unip/br/unimetro/tasks/LogTask.java
@@ -1,46 +1,46 @@
-package unicluster.unip.br.unimetro.tasks;
+	package unicluster.unip.br.unimetro.tasks;
 
-import android.content.Context;
-import android.os.AsyncTask;
+	import android.content.Context;
+	import android.os.AsyncTask;
 
-import unicluster.unip.br.unimetro.web.Client;
+	import unicluster.unip.br.unimetro.web.Client;
 
-public class LogTask extends AsyncTask<Void, Integer, String> {
+	public class LogTask extends AsyncTask<Void, Integer, String> {
 
-	private Context context;
+		private Context context;
 
-	private TaskComplete delegate;
+		private TaskComplete delegate;
 
-	private Client client;
+		private Client client;
 
-	private int line = -1;
+		private int line = -1;
 
-	public LogTask(Context context, TaskComplete delegate) {
-		this.context = context;
-		this.delegate = delegate;
-		this.client = new Client();
-	}
+		public LogTask(Context context, TaskComplete delegate) {
+			this.context = context;
+			this.delegate = delegate;
+			this.client = new Client();
+		}
 
-	public LogTask(Context context, TaskComplete delegate, int line) {
-		this.context = context;
-		this.delegate = delegate;
-		this.line = line;
-		this.client = new Client();
-	}
+		public LogTask(Context context, TaskComplete delegate, int line) {
+			this.context = context;
+			this.delegate = delegate;
+			this.line = line;
+			this.client = new Client();
+		}
 
 
-	@Override
-	protected String doInBackground(Void... voids) {
-		if (this.line != -1) {
-			return  client.logs(this.line);
-		} else {
-			return client.logs();
+		@Override
+		protected String doInBackground(Void... voids) {
+			if (this.line != -1) {
+				return  client.logs(this.line);
+			} else {
+				return client.logs();
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			//super.onPostExecute(result);
+			delegate.onTaskCompleted(result);
 		}
 	}
-
-	@Override
-	protected void onPostExecute(String result) {
-		//super.onPostExecute(result);
-		delegate.onTaskCompleted(result);
-	}
-}
